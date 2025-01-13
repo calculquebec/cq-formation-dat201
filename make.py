bb29e719dd1e7eb8286637995a89f3ad18c34def
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2025-01-13 17:09:43 -0500

    Initial script creating 4 versions from 1 source

---
+++ make.py
@@ -0,0 +1,109 @@
+import argparse
+import json
+import os
+
+languages = ['en', 'fr']
+notebook_files = [
+    '01-dataframe.ipynb',
+]
+
+
+def make_nodebook(src_path, new_path, lang, remove_tag):
+    print(f'Building {new_path} ...')
+
+    with open(src_path, 'r') as ipynb:
+        src_dict = json.load(ipynb)
+
+    new_cells = []
+
+    for cell in src_dict['cells']:
+        cell_metadata = cell['metadata']
+
+        # Skip cells of the other version
+        if 'tags' in cell_metadata and remove_tag in cell_metadata['tags']:
+            continue
+
+        source_code = cell['source']
+
+        # No language required for empty cells
+        if not source_code:
+            new_cells.append(cell)
+            continue
+
+        assert 'lang' in cell_metadata, \
+            f'Cell "{source_code[0]} ..." has no "lang" property'
+        assert cell_metadata['lang'], \
+            f'Cell "{source_code[0]} ..." has empty "lang" property'
+
+        meta_languages = cell_metadata['lang'].split(',')
+
+        for meta_lang in meta_languages:
+            assert meta_lang in languages, \
+                f'Cell "{source_code[0]} ..." has invalid lang "{meta_lang}"'
+
+        if lang in meta_languages:
+            new_cells.append(cell)
+
+    new_dict = {
+        'cells': new_cells,
+        'metadata': src_dict['metadata'],
+        'nbformat': src_dict['nbformat'],
+        'nbformat_minor': src_dict['nbformat_minor'],
+    }
+
+    with open(new_path, 'w') as ipynb:
+        json.dump(new_dict, ipynb, ensure_ascii=False)
+
+    return len(new_cells)
+
+
+def make_nodebooks(src_file, lang, force_rebuild):
+    versions = {
+        'student': {'prefix': '', 'remove_tag': 'soln'},
+        'teacher': {'prefix': 'solution-', 'remove_tag': 'exer'},
+    }
+    nb_cells = {version: 0 for version in versions.keys()}
+
+    src_path = os.path.join('src', src_file)
+    src_mtime = os.path.getmtime(src_path)
+
+    for version, attr in versions.items():
+        dir_name = f"{attr['prefix']}{lang}"
+        new_path = os.path.join(dir_name, src_file)
+
+        if os.path.isfile(new_path):
+            new_mtime = os.path.getmtime(new_path)
+        else:
+            new_mtime = src_mtime - 1
+
+        if (new_mtime < src_mtime) or force_rebuild:
+            nb_cells[version] = make_nodebook(
+                src_path,
+                new_path,
+                lang,
+                attr['remove_tag'])
+
+    assert nb_cells['student'] == nb_cells['teacher'], \
+        f'teacher and student versions have different number of cells ({lang})'
+
+
+def make_all_nodebooks():
+    arg_parser = argparse.ArgumentParser(
+        prog='python make.py',
+        description='Generates material and solution per language')
+
+    arg_parser.add_argument(
+        '-r',
+        '--rebuild',
+        action='store_true',
+        help='Rebuild notebooks')
+
+    args = arg_parser.parse_args()
+
+    for src_file in notebook_files:
+        for lang in languages:
+            make_nodebooks(src_file, lang, args.rebuild)
+
+
+if __name__ == '__main__':
+    make_all_nodebooks()
