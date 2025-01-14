47476155c96ac1f4917711a93a2ca17504c4d8ac
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2025-01-13 20:32:58 -0500

    Some docstrings and new variable names

--- make.py
+++ make.py
@@ -8,7 +8,19 @@ notebook_files = [
 ]
 
 
-def make_nodebook(src_path, new_path, lang, remove_tag):
+def make_nodebook(src_path: str, new_path: str, lang: str, remove_tag: str):
+    '''Update or rebuild a single notebook from the source notebook
+
+    Arguments:
+    src_path -- relative path to the source notebook file
+    new_path -- relative path to the new notebook file
+    lang -- language code, either "en" or "fr"
+    remove_tag -- if this tag is found, remove the cell from the output
+
+    Return value:
+    The final number of cells in the saved file.
+    '''
+
     print(f'Building {new_path} ...')
 
     with open(src_path, 'r') as ipynb:
@@ -57,46 +69,64 @@ def make_nodebook(src_path, new_path, lang, remove_tag):
     return len(new_cells)
 
 
-def make_nodebooks(src_file, lang, force_rebuild):
+def make_nodebooks(src_file: str, lang: str, force_rebuild: bool = False):
+    '''Update or rebuild the student and the teacher notebooks of language lang
+
+    The student version will have the cells with the tag "exer".
+    The teacher version will have the cells with the tag "soln".
+    When there is no specific tag, both versions will have the cell.
+
+    Arguments:
+    src_file -- notebook filename (*.ipynb) expected to be in src/
+    lang -- language code, either "en" or "fr"
+    force_rebuild -- if True, it overrides the modification time test
+    '''
+
     versions = {
-        'student': {'prefix': '', 'remove_tag': 'soln'},
-        'teacher': {'prefix': 'solution-', 'remove_tag': 'exer'},
+        'student': {'dir_prefix': '', 'remove_tag': 'soln'},
+        'teacher': {'dir_prefix': 'solution-', 'remove_tag': 'exer'},
     }
     nb_cells = {version: 0 for version in versions.keys()}
 
     src_path = os.path.join('src', src_file)
     src_mtime = os.path.getmtime(src_path)
 
-    for version, attr in versions.items():
-        dir_name = f"{attr['prefix']}{lang}"
+    for version, attrib in versions.items():
+        dir_name = f"{attrib['dir_prefix']}{lang}"
         new_path = os.path.join(dir_name, src_file)
 
         if os.path.isfile(new_path):
             new_mtime = os.path.getmtime(new_path)
         else:
-            new_mtime = src_mtime - 1
+            new_mtime = 0.0
 
         if (new_mtime < src_mtime) or force_rebuild:
             nb_cells[version] = make_nodebook(
                 src_path,
                 new_path,
                 lang,
-                attr['remove_tag'])
+                attrib['remove_tag'])
 
     assert nb_cells['student'] == nb_cells['teacher'], \
-        f'teacher and student versions have different number of cells ({lang})'
+        f'student and teacher versions have different number of cells ({lang})'
 
 
 def make_all_nodebooks():
+    '''Update or rebuild all notebooks
+
+    All notebooks:
+    - for all source files
+        - for all languages in [en, fr]
+            - for student and teacher versions
+    '''
+
     arg_parser = argparse.ArgumentParser(
         prog='python make.py',
         description='Generates material and solution per language')
 
     arg_parser.add_argument(
-        '-r',
-        '--rebuild',
-        action='store_true',
-        help='Rebuild notebooks')
+        '-r', '--rebuild', action='store_true',
+        help='Force rebuilding notebooks')
 
     args = arg_parser.parse_args()
 
